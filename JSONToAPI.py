4acded271a554853ba28e615bc995d567f4097d0
Jon Boulanger <t-joboul@microsoft.com>
2016-01-13 15:41:25 -0800

    cool

--- JSONToAPI.py
+++ JSONToAPI.py
@@ -3,11 +3,15 @@
 2. Train the mlb person group 
 '''
 from projectoxford.Client import Client
+from projectoxford.Person import Person
+from projectoxford.PersonGroup import PersonGroup
 import json
 import time
 
 client = Client()
 faceClient = client.face('9defe6b7bd8045eea6cccb8e4b8d5ddd')
+personClient = Person('9defe6b7bd8045eea6cccb8e4b8d5ddd') 
+
 
 def getJSONfromDB(path): # readDB :: String -> JSON
     ''' Gets the JSON from the file at the inputted path and returns it as a string'''
@@ -24,6 +28,7 @@ def addPerson(name, url, personGroup): # addPerson :: String -> String -> String
     ''' Creates a person based in an inputted name and image url before adding it to the inputted personGroup in Project Oxford's API'''
     # Detect face from image
     result = faceClient.detect({'url': url})
+    print result
     faceId = result[0]['faceId']
     # Create person
     faceClient.person.createOrUpdate(personGroup, [faceId], name)
@@ -33,7 +38,7 @@ def addPeople(people, groupName): # addPeople :: JSON -> String -> ()
     # Iterate through the person Dictionary
     for person in people:
         # Add this specific person to personGroup
-        addPerson(person['name'], person['url'], groupName)
+        addPerson(person['name'], person['image_url'], groupName)
         # Log addition of person
         print (person['name'] + ' was added to the ' + groupName + ' personGroup')
         # Wait 3 seconds in between each person to avoid throttling
@@ -41,9 +46,11 @@ def addPeople(people, groupName): # addPeople :: JSON -> String -> ()
 
 def main():
     # Add people to person group
-    group = 'example-person-group'
-    addPeople(getJSONfromDB('db.json'), group)
+    group = 'mlb'
+    #faceClient.personGroup.create(group, group)   
+    #addPeople(getJSONfromDB('db.json'), group)
+    print faceClient.person.list(group)
     # Train person group
-    faceClient.personGroup.trainAndPollForCompletion(group)
+    #faceClient.personGroup.trainAndPollForCompletion(group)
 
 main()
